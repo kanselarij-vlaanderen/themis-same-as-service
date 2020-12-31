21bb3eb2775be6e9db5866e26bb25415c7a707a2
Annelies-P <annelies.pauwels@gmail.com>
2020-12-31 15:01:54 +0100

    VAL-187 same-as mapping for kaleidos minister data

--- config.js
+++ config.js
@@ -1,11 +1,13 @@
 const MU_APPLICATION_GRAPH = 'http://mu.semte.ch/graphs/publication-tasks';
 const SAME_AS_GRAPH = 'http://mu.semte.ch/graphs/same-as';
+const PUBLIC_GRAPH = 'http://mu.semte.ch/graphs/public';
 const SELECT_BATCH_SIZE = parseInt(process.env.SELECT_BATCH_SIZE) || 1000;
 const RENAME_DOMAIN = process.env.RENAME_DOMAIN || 'http://themis.vlaanderen.be/id/resource/';
 
 export {
   MU_APPLICATION_GRAPH,
   SAME_AS_GRAPH,
+  PUBLIC_GRAPH,
   SELECT_BATCH_SIZE,
   RENAME_DOMAIN
 };
